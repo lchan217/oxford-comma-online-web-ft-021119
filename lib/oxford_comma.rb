2a1b459ed3ded7136625f9290350c9361b3490e5
Lizzie Chan <lchan217@gmail.com>
2019-03-06 01:20:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,12 @@ def oxford_comma(array)
     array[0]
     elsif array.length == 2 
     "#{array[0]} and #{array[1]}"
+    elsif array.length > 2 
+    last = array[-1]
+    array.pop
+    array.map do |fruit|
+      "#{fruit}, "
+    end 
+    puts "#{array}and #{last}"
   end
 end 
