32ed5527c2f1927999f72ca261a9886a629cad32
Lizzie Chan <lchan217@gmail.com>
2019-03-06 01:17:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   elsif array.length == 2 
     array.join(" and ")
   else
-    last = array[-1]
+    last = array[-1] #store last element separately 
     array.pop #delete last element
     array.join(", ") + ", and #{last}"
   end 
